9143984f64c09ffe23cb0e39da9d8b1e4b9a8809
Patar Martua Doli Siahaan <123785905+patarmds@users.noreply.github.com>
2023-02-15 09:25:11 +0700

    Update UserController.js

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -23,6 +23,41 @@ module.exports = {
         }
     },
 
+    createUser : async (req, res) => {
+        try{
+            let body = { ...req.body };
+            body.created_at = Date.now();
+            const user = new User(body);
+            const createdUser = await user.save();
+            if(!createdUser) return res.status(400).json(response(false, "Data gagal dibuat", null));
+            return res.status(201).json(response(true, "Data berhasil dibuat", createdUser));
+        }catch(err){
+            return res.status(500).json(response(false, err.message, null));
+        }
+    },
 
+    updateUser : async (req, res) => {
+        try{
+            const id = req.params.id;
+            let body = { ...req.body };
+            body.created_at = undefined;
+            const updatedUser = await User.findOneAndUpdate({_id : id}, body, {new : true});
+            if(!updatedUser) return res.status(400).json(response(false, "Data gagal diedit", null)); 
+            return res.status(200).json(response(true, "Data berhasil diedit", updatedUser));
+        }catch(err){
+            return res.status(500).json(response(false, err.message, null));
+        }
+    },
+
+    deleteUser : async (req, res) => {
+        try{
+            const id = req.params.id;
+            const deletedUser = await User.findByIdAndDelete(id);
+            if(!deletedUser) return res.status(400).json(response(false, "Data gagal dihapus", null)); 
+            return res.status(200).json(response(true, "Data berhasil dihapus", deletedUser));
+        }catch(err){
+            return res.status(500).json(response(false, err.message, null));
+        }
+    }
 
 }
